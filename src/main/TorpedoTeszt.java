3abd697a36fb082060257506c32c68fd8819ab52
hallgato hallgato <hallgato@oktatas.local>
2022-05-27 12:31:16 +0200

    Süllyedés teszt

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -3,26 +3,44 @@ package main;
 import modell.Hajo;
 
 public class TorpedoTeszt {
-    public static void main(String[] args) {
+    
+    
+    private Hajo hajo;
+    
+    public TorpedoTeszt(){
+        hajo = new Hajo(new int[]{2,3,4});
         System.out.println("Teszt");
         
         new TorpedoTeszt().tesztLovesTalalt(4);
         new TorpedoTeszt().tesztLovesNemTalalt(4);
+        new TorpedoTeszt().tesztLovesSullyedt(4);
+    
     }
     
-    public String tesztLovesTalalt(int poz){
-        Hajo hajo = new Hajo(new int[]{2,3,4});
+    public static void main(String[] args) {
+    new TorpedoTeszt();
+    }
+    
+    public void tesztLovesTalalt(int poz){
+        //Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
-        return "";
+        //return "";
     }
     
-     public String tesztLovesNemTalalt (int poz){
+     public void tesztLovesNemTalalt (int poz){
          System.out.println("nem talált tesz:");
-        Hajo hajo = new Hajo(new int[]{2,3,4});
+        //Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
-        return "";
+        //return "";
+    }
+         public void tesztLovesSullyedt (int poz){
+         System.out.println("nem talált tesz:");
+        //Hajo hajo = new Hajo(new int[]{2,3,4});
+        String t = hajo.talalat(poz);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
+        //return "";
     }
     
 }
